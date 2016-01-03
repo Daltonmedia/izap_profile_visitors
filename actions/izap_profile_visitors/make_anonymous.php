cbc94360b7f0a1c5bd3e61c34dd8cb76a80ebb96
Dries <driesdk@gmail.com>
2016-01-03 21:09:39 +0100

    Create make_anonymous.php

---
+++ actions/izap_profile_visitors/make_anonymous.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Make a user anonymous
+ */
+
+$user = elgg_get_logged_in_user_entity();
+
+//only admins for now
+if (!$user->isAdmin()) {
+	register_error(elgg_echo('actionunauthorized'));
+}
+
+$guid = get_input('guid');
+
+$user = get_user($guid);
+
+$user->visanonymity = 'true';
+
+system_message(elgg_echo('izapProfileVisitor:MadeAnonymous', array($user->name)));
