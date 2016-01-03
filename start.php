eb049eadd8272a5498d4b6656624594f85ee9757
Dries <driesdk@gmail.com>
2016-01-03 21:39:55 +0100

    Update start.php

--- start.php
+++ start.php
@@ -18,7 +18,7 @@ function izapProfileVisitors() {
 	elgg_register_library('izap_profile_visitors_library', elgg_get_plugins_path() . 'izap_profile_visitors/lib/izap_profile_visitors_lib.php');
 	elgg_load_library('izap_profile_visitors_library');
 	
-	$action_path = dirname(__FILE__) . '/actions/';
+	$action_path = dirname(__FILE__) . '/actions/izap_profile_visitors/';
 	elgg_register_action('make_anonymous', $action_path . 'make_anonymous.php', 'admin');
 	elgg_register_action('unmake_anonymous', $action_path . 'unmake_anonymous.php', 'admin');
 
